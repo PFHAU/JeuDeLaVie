5efc08fcbdc99c15cac0419bda97bef0eec114bb
Hak97 <36701735+Hak97@users.noreply.github.com>
2018-03-07 13:59:21 +0100

    Update Liste.java

--- Liste.java
+++ Liste.java
@@ -1,33 +1,55 @@
-package Projet;
 import java.io.*;
 
 public class Liste {
 
-    public static Maillon first;
+    private Maillon first;
 
     public Liste (){
         first=null;
 
+    }
+    
+    public void add(Coordoner p){
+    	Maillon c = new Maillon(p);
+    	if (this.vide()){
+    		this.first=c;
+    	}else{
+    		Maillon tmp=this.first;
+    		if(tmp.compareTo(c)>=0){
+    			c.setSuivant(tmp);
+    			this.first=c;
+    		}else{
+    			if(tmp.getSuivant()==null){
+    				tmp.setSuivant(c);
+    			}else{
+    				boolean b= true;
+    				while(tmp.getSuivant()!=null){
+    					if(tmp.getSuivant().compareTo(c)<0){
+    						tmp=tmp.getSuivant();
+    					}else{
+    						c.setSuivant(tmp.getSuivant());
+    						tmp.setSuivant(c);
+    						b=false;
+    					}
+    				}
+    				if (b){
+    					tmp.setSuivant(c);
+    				}
+    			}
+    		}
+    	}
     }
 
     public boolean vide(){
         return first == null;
     }
 
-    public static Maillon getFirst() {
+    public Maillon getFirst() {
         return first;
     }
 
-    public static void setFirst(Maillon first) {
-        Liste.first = first;
-    }
-
-    public String toString(){
-        String string="";
-        for(Maillon m= first; m!=null; m=m.suivant){
-            string=string +String.valueOf(m.c)+""; //valueOf pour transformer en String
-        }
-        return string;
+    public void setFirst(Maillon first) {
+        this.first = first;
     }
 
 }
