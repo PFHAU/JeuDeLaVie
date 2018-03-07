a4f9009a83f6d5f30eb90c0fd4997b2e6623abf3
Hak97 <36701735+Hak97@users.noreply.github.com>
2018-03-07 13:57:01 +0100

    Update Coordoner.java

--- Coordoner.java
+++ Coordoner.java
@@ -1,12 +1,33 @@
-package Projet;
-
 public class Coordoner {
-
-    int x;
-    int y;
+    private int x;
+    private int y;
     public  Coordoner (int x,int y){
         this.x=x;
         this.y=y;
     }
+    public int getX(){
+    	return x;
+    }
+    public int getY(){
+    	return y;
+    }
+    public int compareTo(Coordoner c){
+    	if(this.getY()<c.getY()){
+    		return -1;
+    	}else{
+    		if (this.getY()>c.getY()){
+    			return 1;
+    		}else{
+    			if (this.getX()<c.getX()){
+    				return -1;
+    			}else{
+    				if(this.getX()>c.getX()){
+    					return 1;
+    				}else{
+    					return 0;
+    				}
+    			}
+    		}
+    	}
+    }
 }
-
