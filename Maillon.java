1a95a4d5b51bd7c6a4e6097eb1de7406be18d22a
Hak97 <36701735+Hak97@users.noreply.github.com>
2018-03-07 13:59:58 +0100

    Update Maillon.java

--- Maillon.java
+++ Maillon.java
@@ -1,43 +1,28 @@
-package Projet;
-
 public class Maillon {
-    Coordoner c;
-    Maillon suivant;
-    public Maillon (Coordoner c,Maillon suivant){
-        this.c=c;
-        this.suivant=suivant;
-    }
-    public int compareTo (Maillon m) {
-        if (m.c.x < this.c.x) {
-            return 1;
-        } else {
-            if (this.c.x < m.c.x) {
-                return -1;
-            } else {
-                if (this.c.x == m.c.x) {
-                    if (m.c.y < this.c.y) {
-                        return 1;
-                    }
-                    if (this.c.y < m.c.y) {
-                        return -1;
-                    }
-                    if (this.c.x == m.c.x) {
-                        return 0;
-                    }
-                }
-            }
-        } return 404;
-    }
+	private Object o;
+	private Maillon suivant;
+	public Maillon (Object o){
+		this.o=o;
+		this.suivant=null;
+	}
+	public Object getO() {
+		return o;
+	}
 
-    public boolean estDans(Liste l){
-        Maillon tmp=l.first;
-        while (tmp.suivant.compareTo(null)==0){
-            if (tmp.compareTo(this)==0){
-                return true;
-            }
-            tmp=tmp.suivant;
-        }
-        return false;
-    }
+	public void setO(Object o) {
+		this.o = o;
+	}
+
+	public Maillon getSuivant() {
+		return suivant;
+	}
+
+	public void setSuivant(Maillon suivant) {
+		this.suivant = suivant;
+	}
+	public int compareTo(Object obj){
+		return ((Maillon) this.o).compareTo(((Maillon) obj).o);
+	}
+
+
 }
-
