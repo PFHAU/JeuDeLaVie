a945e02165e0d0e601b58779cc7ceecf76a58bb6
PFHAU <36701332+PFHAU@users.noreply.github.com>
2018-03-24 13:04:09 +0100

    Add files via upload

--- Coordoner.java
+++ Coordoner.java
@@ -8,5 +8,33 @@ public class Coordoner {
         this.x=x;
         this.y=y;
     }
+
+    public void augmenterX(int i) {
+        this.x=x+i;
+    }
+
+    public void augmenterY(int i){
+        this.y=y+i;
+    }
+
+    public int compareTo(Coordoner d){// a Faire, ici c'est un test.
+        return -1;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
 
