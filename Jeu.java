a73ceab67fa5ba0aeda91403f6b4270e8b53c9ec
Hak97 <36701735+Hak97@users.noreply.github.com>
2018-03-28 14:29:33 +0200

    Create Jeu.java

---
+++ Jeu.java
@@ -0,0 +1,51 @@
+public class Jeu {
+	public static void main(String [] args){
+		Liste l = new Liste();
+		Coordoner p1= new Coordoner(4,-3);
+		Coordoner p2= new Coordoner(6,-5);
+		Coordoner p3= new Coordoner(0,0);
+		Coordoner p4= new Coordoner(0,3);
+		Coordoner p5= new Coordoner(1,-2);
+		l.add(p1);
+		l.add(p2);
+		l.add(p3);
+		l.add(p4);
+		l.add(p5);
+		Coordoner start= new Coordoner(-5,6);
+		afficher(l,start);
+	}
+	
+	public static void afficher(Liste liste, Coordoner p){
+		boolean b = true;
+		boolean f = true;
+		Maillon tmp= liste.getFirst();
+		Maillon mp= new Maillon(p);
+		Maillon m=null;
+		while(tmp.getSuivant()!=null && f){
+			if(tmp.compareTo(mp)<=0){
+				m=tmp;
+				f=false;
+			}else{
+				tmp.getSuivant();
+			}
+		}
+		for(int i=p.getY(); i<p.getY()-10; i++){
+			for(int j=p.getX(); j<p.getX()+10; j++){
+				Maillon z = new Maillon(new Coordoner(j,i));
+				if(m==null){
+					System.out.print(".");
+				}else{
+					if(z.compareTo(m)>0){
+						System.out.print(".");
+					}else{
+						if(z.compareTo(m)==0){
+							System.out.print("*");
+							m=m.getSuivant();
+						}
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+}
