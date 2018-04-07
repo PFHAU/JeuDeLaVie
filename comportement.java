29340ef57c86261b039c0e35c46ca9ed222abaf9
sowelhadji <36701463+sowelhadji@users.noreply.github.com>
2018-04-07 21:48:08 +0200

    Add files via upload

---
+++ comportement.java
@@ -0,0 +1,40 @@
+public class comportement {
+
+    int  taille_generationFisrt = 0;
+    int  taille_generationSuivant = 0;
+
+    //on declare deux generations
+
+    Liste generationFirst;
+    Liste generationSuivant;
+
+    //Avancementdes generations en fonction de leur pas
+    //tant que la liste n'est pas vide
+
+    if(Liste.Maillon != estVide){
+
+    }
+     if(Liste.Methode.evolution.getO.getX()==1){
+
+    }
+
+    public comportement() {
+        generationFirst = Liste.Methode.first;
+        generationSuivant = Liste.Methode.suivant;
+    }
+
+    generationFirst.Suivant;
+         taille_generationFisrt ++;
+
+         Methode.evolution.getO.getY()==2;
+
+        generationSuivant.Suivant.Suivant;
+        taille_generationSuivant ++;
+
+   //sarreter car les deux generations sont egales
+    //
+    generationFirst == generationSuivant;
+
+
+
+}
