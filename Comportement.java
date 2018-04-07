85dd9de6efdd751f1c45b4b156ac6ac21e79d372
sowelhadji <36701463+sowelhadji@users.noreply.github.com>
2018-04-07 21:50:00 +0200

    Add files via upload

---
+++ Comportement.java
@@ -0,0 +1,83 @@
+package Classes;
+
+
+import java.util.Iterator;
+
+public class Comportement {
+
+    int  taille_generationFisrt = 0;
+    int  taille_generationSuivant = 0;
+    int  taille_queue = 0;
+
+    //on declare deux generations
+
+    Liste generationX;
+    Liste generationY;
+    // univers doit etre creer a l'avance
+    private Univer a;
+
+
+    public int calculePeriode(Univer a){
+        this.a = a;
+        int periode =1;
+        //ici on doit creer univers pour pouvoir iterer passer de generation a une autre
+        //pour pouvoir mettre ici it.hasNext
+        while (Liste.suivant){
+            liste.add(x,y);
+
+        }
+        // it.next
+        x.nextGeneration();
+
+        while(generationX!=generationY) {
+            y.nextGeneration();
+            periode++;
+        }
+
+
+    return periode;
+    }
+
+
+
+    public void calculTailles(){
+  
+
+    //Avancementdes generations en fonction de leur pas
+    //tant que la liste n'est pas vide
+
+    private final Generation Periode;
+
+
+     if(Methodes.evolution().getO.getX()==1)
+
+    {
+
+
+
+        if (Periode.Generation())
+
+        {
+            generationFirst.Suivant
+            taille_generationFisrt++;
+
+
+            Methodes.evolution.getClass().getY() == 2;
+
+            generationSuivant.Suivant.Suivant;
+            taille_generationSuivant++;
+        }
+        Object periode;
+        periode.suivant;
+        taille_queue++;
+
+    }
+    //sarreter car les deux generations sont egales
+    //
+    generationFirst == generationSuivant;
+
+
+
+}
+
+}
