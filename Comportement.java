dc992f53f6880552fb09a2720a7a83c9c11f9f3f
sowelhadji <36701463+sowelhadji@users.noreply.github.com>
2018-05-04 00:16:01 +0200

    Add files via upload

--- Comportement.java
+++ Comportement.java
@@ -11,26 +11,41 @@ public class Comportement {
 
     //on declare deux generations
 
-    Liste generationX;
-    Liste generationY;
+    Liste generationX = new Liste();
+    Liste generationY = new Liste();
+
+    int periode;
+
+    Liste l;
+
+    Liste genX = l;
+
+    Liste genY = l;
     // univers doit etre creer a l'avance
-    private Univer a;
+    private Liste a;
 
 
-    public int calculePeriode(Univer a){
-        this.a = a;
+
+    public int calculePeriode(Liste a){
+
+        Liste<Maillon> liste = new Liste();
+
+
+
         int periode =1;
+        Iterator<Maillon> it = null Methodes.evolution();
         //ici on doit creer univers pour pouvoir iterer passer de generation a une autre
         //pour pouvoir mettre ici it.hasNext
-        while (Liste.suivant){
-            liste.add(x,y);
+        while (it.suivant()){
+            Maillon celluleI = Maillon.it.suivant();
+            liste.add(Coordonner.x,Coordonner.y);
 
         }
         // it.next
-        x.nextGeneration();
-
+        it.hasNext();//it.NextGeneration();
+ 
         while(generationX!=generationY) {
-            y.nextGeneration();
+            it.hasNext();
             periode++;
         }
 
@@ -38,45 +53,33 @@ public class Comportement {
     return periode;
     }
 
+    public int taille8queue(){
+
+        int taille_queue=0;
+
+        while (periode > 1){
+            genX= genX.generationX;
+            genY= genY.generationY;
 
 
-    public void calculTailles(){
-  
-
-    //Avancementdes generations en fonction de leur pas
-    //tant que la liste n'est pas vide
-
-    private final Generation Periode;
-
-
-     if(Methodes.evolution().getO.getX()==1)
-
-    {
-
-
-
-        if (Periode.Generation())
-
-        {
-            generationFirst.Suivant
-            taille_generationFisrt++;
-
-
-            Methodes.evolution.getClass().getY() == 2;
-
-            generationSuivant.Suivant.Suivant;
-            taille_generationSuivant++;
+            taille_queue ++ ;
         }
-        Object periode;
-        periode.suivant;
-        taille_queue++;
-
+        return taille_queue;
     }
+
+
     //sarreter car les deux generations sont egales
     //
-    generationFirst == generationSuivant;
+        public boolean Siidentique(Liste<Maillon>) {
+        boolean identique;
+        int GoX = Liste.suivant().getCoorX;
+        int GoY = Liste.suivant().getCoorY;
+        identique = true;
 
-
+        while (suivant() && identique) {
+            Methodes.evolution();
+        }
+        return identique;
 
 }
 
